58f71dbf83ea8f1c9fa3bcfc75b9d369308f0934
msongkiet <33480228+msongkiet@users.noreply.github.com>
2021-08-06 18:53:25 +0700

    Update dump_trained_model.py

--- dump_trained_model.py
+++ dump_trained_model.py
@@ -14,10 +14,10 @@ print(df)
 model = linear_model.LinearRegression()
 model.fit(df[['area']], df.price)
 
-# print(f'Coefficient : {model.coef_}')
-# print(f'Intercept : {model.intercept_}')
+print(f'Coefficient : {model.coef_}')
+print(f'Intercept : {model.intercept_}')
 
-model.predict([[5000]])
+print(model.predict([[5000]]))
 
 # dump trained model
 from joblib import dump
